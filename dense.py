bug fixes and softmax forward/backward implementation

dense.py
@@ -16,25 +16,31 @@ class Dense:
     def forward(self, x): # x = input/immagine, y = output
         self.x = x # salvo in memoria l'input (per backward)
         # generazione dell'output usando la funzione y = b + W*x
-        self.y = self.bias + x @ self.weights # varianza He per ReLU
+        y = self.bias + x @ self.weights # varianza He per ReLU
         # funzione di attivazione per dare non linearità
         # più avanti si può mettere un if con tutte le funzioni di attivazione
         if self.activation_fn == 'relu':
-            y = np.maximum(self.y, 0)
+            self.y = np.maximum(y, 0)
         elif self.activation_fn == 'softmax':
-            y = np.exp(self.y) / np.sum(np.exp(self.y), axis=1, keepdims=True)
+            exps = np.exp(y - np.max(y, axis=1, keepdims=True)) # evito la cancellazione numerica sottraendo
+            self.y = exps / np.sum(exps, axis=1, keepdims=True) # il valore massimo
         elif self.activation_fn == None:
             pass
         else:
             raise ValueError('Activation function passata non valida')
         
-        return y
+        return self.y
     
     def backward(self, d_y): # derivata rispetto all'output (si usa chain rule)
         if self.activation_fn == 'relu':
-            d_y = d_y / (np.heaviside(self.y) + 10**(-8))
-        elif self.activation_fn == 'softmax':
-            pass
+            d_y = d_y * (self.y > 0) # dy/dx=1 se y>0, dW/dx=dW/dy*dy/dx
+        elif self.activation_fn == 'softmax': # per la softmax c'è da calcolare la jacobiana
+            batch_size = d_y.shape[0] # visto che la derivata non è semplicemente y/x
+                                      # bisogna calcolarla per ogni immagine della batch
+            for i in range(batch_size):
+                prob = self.y[i].reshape(-1, 1) # tolgo una dimensione superflua
+                d_softmax = np.diagflat(prob) - np.dot(prob, prob.T) # jacobiana della softmax
+                d_y[i] = d_y[i] @ d_softmax
         elif self.activation_fn == None:
             pass
         else:
@@ -71,7 +77,7 @@ class SimpleMLP:
 
         self.layers = np.array(self.layers)
         # learning rate
-        self.lr = 0.001 # per una rete piccola 10^3 va bene, ma può essere meno
+        self.lr = 0.001 # per una rete piccola 10^-3 va bene, ma può essere meno
     
     def forward(self, x):
         for l in self.layers:

test.py
@@ -2,12 +2,12 @@ import pickle
 import dense as d
 import MNIST_dataloader as mnist
 import numpy as np
-import cv2
+#import cv2
 
 z = 3
 
-loader = mnist.MnistDataloader('CNN\\mnist\\train_images.idx3-ubyte', 'CNN\\mnist\\train_labels.idx1-ubyte',
-                                'CNN\\mnist\\test_images.idx3-ubyte', 'CNN\\mnist\\test_labels.idx1-ubyte')
+loader = mnist.MnistDataloader('mnist\\train_images.idx3-ubyte', 'mnist\\train_labels.idx1-ubyte',
+                                'mnist\\test_images.idx3-ubyte', 'mnist\\test_labels.idx1-ubyte')
 
 (x_train, y_train), (x_test, y_test) = loader.load_data()
 (x_train, y_train), (x_test, y_test) = (np.array(x_train), np.array(y_train)), (np.array(x_test), np.array(y_test))
@@ -20,7 +20,7 @@ y_test = np.reshape(y_test, (-1, 1))
 
 del x_train, y_train
 
-with open("model_mkIII.pickle", "rb") as handle:
+with open("models\\model.pickle", "rb") as handle:
     model = pickle.load(handle)
 
 x = np.reshape(x_test, (-1, 28*28))

train.py
@@ -10,8 +10,8 @@ from sklearn.utils import shuffle
 
 mlp = d.SimpleMLP()
 
-loader = mnist.MnistDataloader('CNN\\mnist\\train_images.idx3-ubyte', 'CNN\\mnist\\train_labels.idx1-ubyte',
-                                'CNN\\mnist\\test_images.idx3-ubyte', 'CNN\\mnist\\test_labels.idx1-ubyte')
+loader = mnist.MnistDataloader('mnist\\train_images.idx3-ubyte', 'mnist\\train_labels.idx1-ubyte',
+                                'mnist\\test_images.idx3-ubyte', 'mnist\\test_labels.idx1-ubyte')
 
 (x_train, y_train), (x_test, y_test) = loader.load_data()
 (x_train, y_train), (x_test, y_test) = (np.array(x_train), np.array(y_train)), (np.array(x_test), np.array(y_test))
@@ -45,7 +45,7 @@ for epoch in tqdm(range(total_epochs), desc='Training process'):
     remaining_time = (total_epochs - (epoch + 1)) * (elapsed_time/(epoch + 1))
     tqdm.write(f'Epoch {epoch + 1}/{total_epochs} completed, loss: {loss}, ETA: {remaining_time:.2f} s')
 
-with open('model_mkIV.pickle', 'wb') as handle:
+with open('models\\model.pickle', 'wb') as handle:
     pickle.dump(mlp, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
 '''